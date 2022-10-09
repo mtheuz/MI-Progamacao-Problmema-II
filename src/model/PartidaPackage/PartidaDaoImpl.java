36ee10211363483b07022acd9511b00a1997d984
mailsonalves <mailsonalves.new@gmail.com>
2022-10-09 20:51:21 -0300

    Cadastro de grupos e geração de partidas

--- src/model/PartidaPackage/PartidaDaoImpl.java
+++ src/model/PartidaPackage/PartidaDaoImpl.java
@@ -3,15 +3,19 @@ package model.PartidaPackage;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+
+import model.SelecaoPackage.Selecao;
 
 
 
 public class PartidaDaoImpl implements PartidaDAO{
 	public List<Partida> partidas;
 	
-	PartidaDaoImpl(){
-		this.partidas = new ArrayList<Partida>();
+	public PartidaDaoImpl(){
+		this.partidas = new ArrayList<Partida>(); 
 	}
 	@Override
 	public void inserir() {
@@ -38,5 +42,60 @@ public class PartidaDaoImpl implements PartidaDAO{
 	    String coddate = dataagr.format(formato);
 		return coddate;
 	}
+	
+	public Map<String, ArrayList<Partida>> gerarPartidas(Map<String, ArrayList<Selecao>> grupos) {
+		
+		
+		Map< String,ArrayList <Partida> > partidas = new HashMap<String, ArrayList <Partida>>();
+	
+		partidas.put("A", new ArrayList<Partida>());
+		partidas.put("B", new ArrayList<Partida>());
+		partidas.put("C", new ArrayList<Partida>());
+		partidas.put("D", new ArrayList<Partida>());
+		partidas.put("E", new ArrayList<Partida>());
+		partidas.put("F", new ArrayList<Partida>());
+		partidas.put("G", new ArrayList<Partida>());
+		partidas.put("H", new ArrayList<Partida>());
+		
+		for(String grupo: grupos.keySet())
+		{
+			ArrayList<Selecao> lista = grupos.get(grupo);
+			for(Selecao selecao: lista)
+			{
+				String selecao1 = selecao.getNome();
+				for(Selecao selecaoo: lista)
+				{
+					String selecao2 = selecaoo.getNome();
+					if(existeJogo(selecao1, selecao2, partidas.get(grupo))== false)
+					{
+						Partida novaPartida = new Partida();
+						novaPartida.setSelecao1(selecao1);
+						novaPartida.setSelecao1(selecao2);
+						partidas.get(grupo).add(novaPartida);
+					}
+				}
+			}
+		}
+		
+		
+		return partidas;
+	}
+	private boolean existeJogo(String selecao1, String selecao2, ArrayList<Partida> listaDePartidas) {
+		if(listaDePartidas.size()>0)
+		{
+			for(Partida partida : listaDePartidas)
+			{
+				if(!(partida.getSelecao1() == selecao1 || partida.getSelecao1() == selecao2 && partida.getSelecao2() == selecao1 || partida.getSelecao2() == selecao2 ))
+				{
+					return true;
+					
+				}
+				return false;
+			}
+		}
+		return false;
+	}
+	
+	}
 
-}
+
